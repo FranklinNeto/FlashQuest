ab01d7fee5e7e8440100cd201d8ed286553c72a3
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2025-03-20 03:39:55 -0400

    add create Stack Navigation

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Testando!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import { NavigationContainer } from "@react-navigation/native"
+//import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { createStackNavigator } from '@react-navigation/stack';
+import { DeckProvider } from "./context/DeckContext"
+import HomeScreen from "./screens/HomeScreen"
+import CreateDeckScreen from "./screens/CreateDeckScreen"
+import FlashcardScreen from "./screens/FlashcardScreen"
+import AddCardScreen from "./screens/AddCardScreen"
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Testando!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <DeckProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: "FlashQuest" }} />
+          <Stack.Screen name="CreateDeck" component={CreateDeckScreen} options={{ title: "Criar Deck" }} />
+          <Stack.Screen name="AddCard" component={AddCardScreen} options={{ title: "Adicionar Flashcard" }} />
+          <Stack.Screen
+            name="Flashcard"
+            component={FlashcardScreen}
+            options={{ title: "Estudar Flashcards", headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </DeckProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
